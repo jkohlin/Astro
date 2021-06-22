ce9f9a4900fdc4c939b1e1ed746e56aaf68a94c5
Johan Kohlin <johan.kohlin@moderskeppet.se>
2021-06-22 23:55:19 +0200

    second commit

---
+++ src/components/VueComp.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h2>This is a {{variable}} component</h2>
+        <p>I just imported it into the astro page. It's as easy as: <pre>import VueComp from '../components/VueComp.vue';</pre></p>
+        <p>here's a prop from the astro page:  {{fromAstro}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                variable: 'vue',
+            }
+        },
+        props: {
+            fromAstro: {
+                type: String,
+            },
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+    h2 {
+        color:white;
+        background-color:red;
+    }
+</style>
---
+++ src/pages/about.md
@@ -0,0 +1,15 @@
+# Hhey there! 
+As you can see I got a new -ishh computer and it types double Hh instead of G!
+## about this page...
+so this page is actually just a straighht md-file. 
+Tihs is what thhe content looks like:
+```
+# Hhey there! 
+As you can see I got a new -ishh computer and it types double Hh instead of G!
+## about this page...
+so this page is actually just a straighht md-file. 
+Tihs is what thhe content looks like:
+(except the code part, that would hhave been like inception...)
+[back to main](/)
+```
+[back to main](/)
